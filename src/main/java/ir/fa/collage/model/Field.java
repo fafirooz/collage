b4300d8ed11f8f0a37ea90b31fbcea090e25b896
fafirooz <fa.firooz@gmail.com>
2023-06-26 22:37:08 -0700

    define

--- src/main/java/ir/fa/collage/model/Field.java
+++ src/main/java/ir/fa/collage/model/Field.java
@@ -6,4 +6,7 @@ public enum Field {
 // enum =  is a special "class" that represents a group of constants.
 // To create an enum, use the enum keyword (instead of class or interface),
 // and separate the constants with a comma. Note that they should be in uppercase letters.
+// math : و یا باقی یک آبجکت از جنس فیلد هستند
+// enum:   کلاسی است( برای همین آیکونش آبی هست مثل کلاس ولی اینترفیس سبزه مشخص میشه که کلاس نیست)/ که مقادیر آن ثابت است و هر زمان مقادیر ثابتی داشتیم
+// ازش استفاده میکنیم. در واقع میشه به جای اینام یک کلاس ساخت که کانستراکتورش رو پرایوت کرد که از بیرون قابل تغییر نباشه و هر کدوم از مقادیر ثابت رو نیو میکنیم.
 
